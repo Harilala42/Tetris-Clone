9a701f519f6b40f8cc6d0da4fcfc88be2c6570c8
Niriantsoa Harilala Rasamimanana <niriantsoaharilala@gmail.com>
2025-09-10 13:36:28 +0300

    Deployment on Render

--- src/Tetris.js
+++ src/Tetris.js
@@ -257,16 +257,18 @@ function playSound(sound, loop) {
 }
 
 function displayMessage(msg, size, position) {
-    ctx.lineWidth = 5;
-    ctx.fillStyle = 'black';
-    ctx.strokeStyle = 'white';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    const positionX = width / 2;
-    const positionY = height * position;
-    ctx.font = `${size}rem 'Chewy'`;
-    ctx.strokeText(msg, positionX, positionY);
-    ctx.fillText(msg, positionX, positionY);
+    document.fonts.ready.then(() => {
+        ctx.lineWidth = 5;
+        ctx.fillStyle = 'black';
+        ctx.strokeStyle = 'white';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        const positionX = width / 2;
+        const positionY = height * position;
+        ctx.font = `${size}rem 'Chewy'`;
+        ctx.strokeText(msg, positionX, positionY);
+        ctx.fillText(msg, positionX, positionY);
+    });
 }
 
 function pauseGame() {
